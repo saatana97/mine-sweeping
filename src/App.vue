aceaf986e87e077235cfa096e9ca74a19249a63c
xiangwk <1041367524@qq.com>
2019-06-17 09:41:29 +0800

    修复一些问题，增加难度，增加金手指

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<div class="header">
 			<p>{{mine-flag}}</p>
-			<button @click="restart">当前难度：{{level+1}}</button>
+			<button @click="restart(false)">当前难度：{{level+1}}</button>
 			<p>{{time}}</p>
 		</div>
 		<div class="center" :style="centerStyle">
@@ -13,15 +13,15 @@
 				:style="boxStyle"
 				:class="handleBoxClass(item)"
 				v-tap="e=>handleTapBox(index)"
-				v-longtap="e=>handleLongBox(item,index)"
-				v-swipeup="e=>handleLongBox(item,index)"
-				v-swipedown="e=>handleLongBox(item,index)"
-				v-swipeleft="e=>handleLongBox(item,index)"
-				v-swiperight="e=>handleLongBox(item,index)"
+				v-longtap="e=>handleLongBox(index)"
+				v-swipeup="e=>handleLongBox(index)"
+				v-swipedown="e=>handleLongBox(index)"
+				v-swipeleft="e=>handleLongBox(index)"
+				v-swiperight="e=>handleLongBox(index)"
 			>
 				<p v-if="item.value===-1 && over">{{item.status===2?'¤':'⊙'}}</p>
-				<p v-if="item.status === 1">{{item.value}}</p>
-				<p v-if="item.status === 2">※</p>
+				<p v-else-if="item.status === 1">{{item.value}}</p>
+				<p v-else-if="item.status === 2">※</p>
 			</div>
 		</div>
 		<div class="bottom">
@@ -29,7 +29,7 @@
 			<p
 				v-for="(item,index) in opts"
 				:key="index"
-				@click="level=index;restart()"
+				@click="level=index;restart(false)"
 			>{{`${item.row}行${item.col}列${item.mine}个地雷，最佳成绩${item.scope}秒`}}</p>
 			<h6>❤❤❤最爱小怂怂的土鸡</h6>
 		</div>
@@ -51,37 +51,73 @@ export default {
 			time: 0,
 			level: 0,
 			boxs: [],
+			life: 0,
 			over: false,
 			init: false,
 			clearId: null,
 			opts: [
-				{ row: 20, col: 15, mine: 30, scope: localStorage.scope1 || 0 },
-				{ row: 30, col: 20, mine: 60, scope: localStorage.scope2 || 0 },
-				{ row: 40, col: 30, mine: 100, scope: localStorage.scope3 || 0 }
+				{ row: 20, col: 15, mine: 50, scope: localStorage.scope1 || 0 },
+				{
+					row: 25,
+					col: 18,
+					mine: 100,
+					scope: localStorage.scope2 || 0
+				},
+				{ row: 30, col: 20, mine: 200, scope: localStorage.scope3 || 0 }
 			]
 		};
 	},
 	mounted() {
+		window.addEventListener("beforeunload", e => {
+			let tips = "确定要关闭本游戏吗？";
+			e.returnValue = tips;
+			return tips;
+		});
+		window.addEventListener("error", e => {
+			alert(`出错啦：${e.message}`);
+		});
 		this.restart();
-		alert("轻触=翻开格子\n按住格子往四周拽=插小旗子");
+		let key = window.prompt(
+			"轻触=翻开格子\n按住格子往四周拽=插小旗子",
+			"请输入金手指哦"
+		);
+		if (
+			[
+				"小怂怂",
+				"小瑶瑶",
+				"陈瑶",
+				"陈优秀",
+				"优秀",
+				"我爱土鸡",
+				"土鸡我爱你",
+				"我喜欢你哦"
+			].indexOf(key) !== -1
+		) {
+			this.life = 3;
+		} else {
+			this.life = 0;
+		}
 	},
 	methods: {
-		restart() {
-			clearInterval(this.clearId);
-			const level = this.opts[this.level];
-			this.init = false;
-			this.over = false;
-			this.row = level.row;
-			this.col = level.col;
-			this.size = Math.floor(window.screen.availWidth / this.col);
-			this.width = this.size * 10;
-			this.height = this.width * 2;
-			this.mine = level.mine;
-			this.time = 0;
-			this.flag = 0;
-			this.boxs = [];
-			for (let i = 0; i < this.row * this.col; i++) {
-				this.boxs.push(new Box(0, 0));
+		restart(auto) {
+			auto = auto !== false;
+			if (auto || window.confirm("确定要重新开始吗？")) {
+				clearInterval(this.clearId);
+				const level = this.opts[this.level];
+				this.init = false;
+				this.over = false;
+				this.row = level.row;
+				this.col = level.col;
+				this.size = Math.floor(window.screen.availWidth / this.col);
+				this.width = this.size * this.col;
+				this.height = this.size * this.row;
+				this.mine = level.mine;
+				this.time = 0;
+				this.flag = 0;
+				this.boxs = [];
+				for (let i = 0; i < this.row * this.col; i++) {
+					this.boxs.push(new Box(0, 0));
+				}
 			}
 		},
 		random(min, max) {
@@ -157,28 +193,35 @@ export default {
 				this.handleInit(index);
 			}
 			if (box.status === 0) {
-				box.trigger();
 				if (box.value === 0) {
+					box.trigger();
 					let near = this.near(index);
-					console.info(index, near);
 					near.forEach(item => {
 						this.handleTapBox(item, true);
 					});
 				}
 				if (box.value === -1) {
-					alert("小怂怂你踩地雷了哦");
-					this.over = true;
+					if (this.life > 0) {
+						box.flag();
+						this.life--;
+						alert(`你还有${this.life}次机会踩到地雷自动插旗哦`);
+					} else {
+						this.over = true;
+						alert("小怂怂你踩地雷了哦");
+					}
 				} else {
+					box.trigger();
 					this.handleover();
 				}
 			}
 		},
-		handleLongBox(item) {
+		handleLongBox(index) {
 			if (this.over) {
 				return false;
 			}
-			item.flag();
-			if (item.status === 2) {
+			let box = this.boxs[index];
+			box.flag();
+			if (box.status === 2) {
 				this.flag++;
 			} else {
 				this.flag--;
